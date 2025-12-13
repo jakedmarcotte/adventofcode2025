63b24ccefa220edf5ee5f79d78d278755662f3f2
Jake Marcotte <jmarcotte@marketingmilk.com>
2025-12-13 09:38:36 -0800

    day 2 part 2

--- day2/day2.go
+++ day2/day2.go
@@ -8,6 +8,60 @@ import (
 	"strings"
 )
 
+func hasRepeatingPattern(s string) bool {
+	// iterate over first half of string
+	for i := 1; i <= len(s)/2; i++ {
+		if len(s)%i != 0 {
+			continue
+		}
+		pattern := s[:i]
+		repeated := true
+		for j := i; j < len(s); j += i {
+			if s[j:j+i] != pattern {
+				repeated = false
+				break
+			}
+		}
+		if repeated {
+			return true
+		}
+	}
+	return false
+}
+
+func part1(invalid_id_sum *int, id_ranges []string) {
+	for _, id_range := range id_ranges {
+		var start, end int
+		fmt.Sscanf(id_range, "%d-%d", &start, &end)
+		for j := start; j <= end; j++ {
+			id_str := strconv.Itoa(j)
+			intLength := len(id_str)
+			if intLength%2 == 0 {
+				midpoint := intLength / 2
+				firstHalf := id_str[:midpoint]
+				secondHalf := id_str[midpoint:]
+				if firstHalf == secondHalf {
+					*invalid_id_sum += j
+				}
+			}
+
+		}
+	}
+}
+
+func part2(invalid_id_sum *int, id_ranges []string) {
+	for _, id_range := range id_ranges {
+		var start, end int
+		fmt.Sscanf(id_range, "%d-%d", &start, &end)
+		for j := start; j <= end; j++ {
+			id_str := strconv.Itoa(j)
+			if hasRepeatingPattern(id_str) {
+				*invalid_id_sum += j
+			}
+		}
+	}
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -16,29 +70,16 @@ func main() {
 	scanner := bufio.NewScanner(data)
 
 	for scanner.Scan() {
-		invalid_id_sum := 0
+		invalid_id_sum_p1 := 0
+		invalid_id_sum_p2 := 0
 		id_list := scanner.Text()
 		id_ranges := strings.Split(id_list, ",")
 
-		for i, id_range := range id_ranges {
-			var start, end int
-			fmt.Sscanf(id_range, "%d-%d", &start, &end)
-			for j := start; j <= end; j++ {
-				id_str := strconv.Itoa(j)
-				intLength := len(id_str)
-				if intLength%2 == 0 {
-					midpoint := intLength / 2
-					firstHalf := id_str[:midpoint]
-					secondHalf := id_str[midpoint:]
-					if firstHalf == secondHalf {
-						invalid_id_sum += j
-					}
-				}
+		part1(&invalid_id_sum_p1, id_ranges)
+		part2(&invalid_id_sum_p2, id_ranges)
 
-			}
-			fmt.Println(id_ranges[i])
-		}
-		fmt.Println(invalid_id_sum)
+		fmt.Println("Twice Repeating Invalid", invalid_id_sum_p1)
+		fmt.Println("2+ Repeating Invalid", invalid_id_sum_p2)
 	}
 
 }
