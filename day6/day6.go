2dfbd2b2df795cce4b3578dc9c945e970cb82ce0
Jake Marcotte <jmarcotte@marketingmilk.com>
2025-12-15 22:08:37 -0800

    finishing day 6 pt 2

--- day6/day6.go
+++ day6/day6.go
@@ -8,31 +8,28 @@ import (
 	"strings"
 )
 
-func main() {
-	data, err := os.Open(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(data)
-
+func part1(lines []string) {
 	// load the list of rolls
-	var number_list [][]int
+	var number_str_list [][]string
+	var max_lengths []int
 	var operators []string
-	var lines int = 0
-	for scanner.Scan() {
-		line := scanner.Text()
+	for _, line := range lines {
 		if strings.Contains(line, "*") || strings.Contains(line, "+") {
 			operators = strings.Fields(line)
 			break
 		}
-		lines++
 		numbers := strings.Fields(line)
 		numSlice := make([]int, 0, len(numbers))
+		max_length := 0
 		for _, numStr := range numbers {
 			num, _ := strconv.Atoi(numStr)
 			numSlice = append(numSlice, num)
+			if len(numStr) > max_length {
+				max_length = len(numStr)
+			}
 		}
-		number_list = append(number_list, numSlice)
+		max_lengths = append(max_lengths, max_length)
+		number_str_list = append(number_str_list, numbers)
 	}
 
 	var results []int
@@ -43,8 +40,9 @@ func main() {
 		} else if ops == "+" {
 			result = 0
 		}
-		for i := 0; i < lines; i++ {
-			value := number_list[i][opIndex]
+		for i := 0; i < len(lines)-1; i++ {
+
+			value, _ := strconv.Atoi(number_str_list[i][opIndex])
 			if ops == "*" {
 				result *= value
 			} else if ops == "+" {
@@ -53,11 +51,86 @@ func main() {
 		}
 		results = append(results, result)
 	}
-	fmt.Println("Results:", results)
 
 	var total int
 	for _, res := range results {
 		total += res
 	}
-	fmt.Println("Total:", total)
+	fmt.Println("Part 1 Total:", total)
+}
+
+func part2(lines []string) {
+	var operators []byte
+	ops := strings.Fields(lines[len(lines)-1])
+	num_of_problems := len(ops)
+	number_list := make([][]int, num_of_problems)
+	current_problem_idx := 0
+	for i := len(lines[0]) - 1; i >= 0; i-- {
+		curr_num := ""
+		next_problem := false
+		// go character by character and add to arrays
+		for j := 0; j <= len(lines)-1; j++ {
+			char := lines[j][i]
+			if char == ' ' {
+				continue
+			} else if char == '*' || char == '+' {
+				operators = append(operators, char)
+				next_problem = true
+			} else {
+				// process digit
+				curr_num += string(char)
+			}
+		}
+		num, _ := strconv.Atoi(curr_num)
+		number_list[current_problem_idx] = append(number_list[current_problem_idx], num)
+		if next_problem {
+			current_problem_idx++
+			i--
+			next_problem = false
+		}
+	}
+
+	var results []int
+	for i := 0; i < num_of_problems; i++ {
+		var result int
+		if operators[i] == '*' {
+			result = 1
+		} else if operators[i] == '+' {
+			result = 0
+		}
+		for j := 0; j < len(number_list[i]); j++ {
+			value := number_list[i][j]
+			if operators[i] == '*' {
+				result *= value
+			} else if operators[i] == '+' {
+				result += value
+			}
+		}
+		results = append(results, result)
+	}
+
+	var total int
+	for _, res := range results {
+		total += res
+	}
+	fmt.Println("Part 2 Total:", total)
+}
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(data)
+
+	// load the list of rolls
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+
+	}
+
+	part1(lines)
+	part2(lines)
 }
